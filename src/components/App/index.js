b74bbfce3a94cbffe2e04a938ed9a3511c543b5e
marionbonin <marion.bonin04@gmail.com>
2022-01-26 21:53:31 +0100

    started CGU page + harmonized all h1 (factorized in main scss)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import Home from '../Home/home';
 import Error from '../Error/error';
 import FAQ from '../FAQ/faq';
 // import Concept from '../Concept/concept';
+import CGU from '../CGU/cgu';
 import Product from '../Product/product';
 
 import './styles.scss';
@@ -19,6 +20,8 @@ function App() {
         <Route path="/connexion" element={<Login />} />
         <Route path="/inscription" element={<CreateAccount />} />
         <Route path="/faq" element={<FAQ />} />
+        <Route path="/CGU" element={<CGU />} />
+        
         {/* <Route path="/concept" element={<Concept />} /> */}
         <Route path="/produit" element={<Product />} />
         <Route path="/*" element={<Error />} />
---
+++ src/components/CGU/cgu.js
@@ -0,0 +1,16 @@
+import Page from '../Page/page';
+import Header from '../Header/header';
+import Footer from '../Footer/footer';
+
+
+export default function GeneralConditions() {
+  return (
+    <>
+      <Header />
+      <Page>
+        <h1>Conditions Générales d'Utilisation</h1>
+      </Page>
+      <Footer />
+    </>
+  );
+}
