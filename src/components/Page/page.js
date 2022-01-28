4b996d907508f279c74abdda9910850df3503558
marionbonin <marion.bonin04@gmail.com>
2022-01-28 13:25:41 +0100

    added possibility to reponsively change body color & having header + page bg managed with opacity

--- src/components/Page/page.js
+++ src/components/Page/page.js
@@ -2,11 +2,21 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const Page = ({ children }) => (
-  <main className="page">
+const Page = ({ children }) => {
+
+  //const screenWidth = window.innerWidth;
+  //const widthOver1200 = (screenWidth > 200) ? true : false;
+  //console.log(screenWidth);
+  //console.log(widthOver500);
+
+  return(
+  <main className="main-wrapper">
+    <div className="page">
     {children}
+    </div>
   </main>
-);
+  )
+};
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
