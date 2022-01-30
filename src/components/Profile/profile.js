02dfe4b4444a66baed486c2cc8fc31c3897d7172
Andrei0Dragomir <andrei.drago.ro@gmail.com>
2022-01-30 13:38:16 +0100

    Page contact terminée, soumission du formulaire et envoi du mail fonctionnels, CSS à peaufiner

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,14 +1,18 @@
+import Container from '@mui/material/Container';
+
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
 import Page from '../Page/page';
 
-export default function Profile {
+export default function Profile () {
 
   return (
     <>
       <Header />
       <Page>
-
+        <Container>
+          <h1>oui</h1>
+        </Container>
       </Page>
       <Footer /> 
     </>
