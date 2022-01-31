181eecb0c8368455cc3a6de8d0e284e028d4022b
Andrei0Dragomir <andrei.drago.ro@gmail.com>
2022-01-31 09:48:59 +0100

    Page de Profile créée en brut, pas de css, pas de fonctionnalités

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,10 +1,15 @@
 import Container from '@mui/material/Container';
 
+
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
 import Page from '../Page/page';
+import FavoriteCard from './FavoriteCard/favoriteCard';
+
+import './style.scss';
 
 export default function Profile () {
+  
 
   return (
     <>
@@ -12,6 +17,20 @@ export default function Profile () {
       <Page>
         <Container>
           <h1>oui</h1>
+          <div>
+            <form  id="profile-form" >
+                <input className="profile-input" type="text" name="user_name" placeholder="Prénom" />
+                <input className="profile-input" type="text" name="user_email" placeholder="Nom" />
+                <input className="profile-input" type="text" name="user_email" placeholder="Promo" />
+                <input className="profile-input" type="email" name="user_email" placeholder="Email" />
+                <input id="button-profile" type="submit" value="Sauvegarde" />
+            </form>
+          </div>
+          <div>
+            <FavoriteCard  key="1" />
+            <FavoriteCard  key="2" />
+            <FavoriteCard  key="3" />
+          </div>
         </Container>
       </Page>
       <Footer /> 
